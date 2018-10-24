ea622b71f481f122dce94b62524205306e4c3eb6
rharish101 <harish.rajagopals@gmail.com>
2018-10-24 12:00:49 +0530

    Added caching of results

--- src/get_dilbert.py
+++ src/get_dilbert.py
@@ -4,48 +4,71 @@ from urllib import request
 import re
 import argparse
 import sys
+from pqdict import pqdict
+import pickle
 
 parser = argparse.ArgumentParser(description="Get a dilbert comic")
 parser.add_argument("comic", type=str, help="certain comic")
 args = parser.parse_args()
 
 FIRST = "1989-04-16"
+FORMAT = "%Y-%m-%d"
+CACHE_FILE = "cache.pickle"
+CACHE_LIMIT = 1000
 
 if args.comic == "latest":
     new_date = datetime.now()
 else:
     try:
-        new_date = datetime.strptime(args.comic[-10:], "%Y-%m-%d")
+        new_date = datetime.strptime(args.comic, FORMAT)
         new_date = min(
-            max(new_date, datetime.strptime(FIRST, "%Y-%m-%d")), datetime.now()
+            max(new_date, datetime.strptime(FIRST, FORMAT)), datetime.now()
         )
     except ValueError:
-        new_date = get_random_date()
+        print("404")
+        sys.exit(1)
+
+
+def key_func(data):
+    return data[-1]
+
+
+try:
+    with open(CACHE_FILE, "rb") as cfile:
+        cache = pickle.load(cfile)
+except FileNotFoundError:
+    cache = pqdict(key=key_func)
+
+if args.comic in cache:
+    if (datetime.now() - cache[args.comic][-1]).days == 0:
+        for item in cache[args.comic]:
+            print(item)
+        sys.exit(0)
 
 is_first = False
 is_latest = False
 
-if new_date.strftime("%Y-%m-%d") == FIRST:
+if new_date.strftime(FORMAT) == FIRST:
     is_first = True
 
 try:
     request.urlopen(
-        "http://dilbert.com/strip/" + datetime.now().strftime("%Y-%m-%d")
+        "http://dilbert.com/strip/" + datetime.now().strftime(FORMAT)
     )
-    latest = datetime.now().strftime("%Y-%m-%d")
+    latest = datetime.now().strftime(FORMAT)
 except request.HTTPError:  # Timezone issues
-    latest = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    latest = (datetime.now() - timedelta(days=1)).strftime(FORMAT)
     if args.comic == "latest":
-        new_date = datetime.strptime(latest, "%Y-%m-%d")
+        new_date = datetime.strptime(latest, FORMAT)
 
-if new_date.strftime("%Y-%m-%d") == latest:
+if new_date.strftime(FORMAT) == latest:
     is_latest = True
 
-left = (new_date + timedelta(days=-1)).strftime("%Y-%m-%d")
-right = (new_date + timedelta(days=1)).strftime("%Y-%m-%d")
+left = (new_date + timedelta(days=-1)).strftime(FORMAT)
+right = (new_date + timedelta(days=1)).strftime(FORMAT)
 
 while True:
-    original = "http://dilbert.com/strip/" + new_date.strftime("%Y-%m-%d")
+    original = "http://dilbert.com/strip/" + new_date.strftime(FORMAT)
     try:
         resp = request.urlopen(original)
         break
@@ -58,11 +81,22 @@ tag = re.findall('<img[^>]*class="img-[^>]*>', html)[0]
 
 url = re.findall('src="[^"]+"', tag)[0][5:-1]
 
-print(url)
-print(original)
-print(FIRST)
-print(left)
-print(right)
-print(latest)
-print(is_first)
-print(is_latest)
+data = [
+    url,
+    original,
+    FIRST,
+    left,
+    right,
+    latest,
+    is_first,
+    is_latest,
+    datetime.now(),
+]
+cache[args.comic] = data
+if len(cache) > CACHE_LIMIT:
+    cache.pop()
+with open(CACHE_FILE, "wb") as cfile:
+    pickle.dump(cache, cfile)
+
+for item in data:
+    print(item)
