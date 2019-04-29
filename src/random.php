c473db40780e0ffa82b4e6dbe06534d6eaef7877
rharish101 <harish.rajagopals@gmail.com>
2019-04-29 07:54:51 +0530

    Fixed date difference in random date generator

--- src/random.php
+++ src/random.php
@@ -3,7 +3,7 @@
 require 'params.php';
 $first = new DateTime(FIRST);  # First dilbert comic ever
 $now = new DateTime('now');
-$span = $now->diff($first, true)->format('%d');
+$span = $now->diff($first, true)->format('%a');
 $days = rand(0, $span);
 $rand_date = $first->add(new DateInterval('P' . $days . 'D'));
 header('LOCATION: ' . $rand_date->format(FORMAT));
