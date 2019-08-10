088da5c7df781d0f926293659824797b37432260
rharish101 <harish.rajagopals@gmail.com>
2019-08-10 15:04:55 +0530

    Cleaned HTML and CSS

--- src/layout.php
+++ src/layout.php
@@ -9,34 +9,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
-  <div id="body" class="text-center">
-
+<body class="text-center">
   <!-- Date and title (if exists) -->
-  <div class="h4 mt-4"><?php echo $data['date_str']; ?></div>
-  <div class="mt-1 h6"><?php echo $data['title']; ?></div>
+  <div class="h4 mx-1 my-2"><?php echo $data['date_str']; ?></div>
+  <div class="h6 m-1"><?php echo $data['title']; ?></div>
 
   <!-- Comic image -->
-  <div class="mt-4 mx-3"><img class="img-fluid" alt="<?php echo $data['actual_date']; ?>" src="<?php echo $data['img_url']; ?>"></img></div>
-  <br>
+  <img class="img-fluid my-3 px-2" alt="<?php echo $data['actual_date']; ?>" src="<?php echo $data['img_url']; ?>"></img>
 
   <!-- Navigation buttons -->
-  <a href="<?php echo FIRST; ?>" role="button" class="btn btn-primary mt-2 mx-1<?php echo $disable_left ? ' disabled' : ''; ?>" aria-disabled="<?php echo $disable_left ? 'true' : 'false'; ?>">&lt&lt</a>
-  <a href="<?php echo $data['left_date']; ?>" role="button" class="btn btn-primary mt-2 mx-1<?php echo $disable_left ? ' disabled' : ''; ?>" aria-disabled="<?php echo $disable_left ? 'true' : 'false'; ?>">&lt</a>
-  <a href="random-comic" role="button" class="btn btn-primary mt-2 mx-1">Random</a>
-  <a href="<?php echo $data['right_date']; ?>" role="button" class="btn btn-primary mt-2 mx-1<?php echo $disable_right ? ' disabled' : ''; ?>" aria-disabled="<?php echo $disable_right ? 'true' : 'false'; ?>">&gt</a>
-  <a href="<?php echo $data['latest_date']; ?>" role="button" class="btn btn-primary mt-2 mx-1<?php echo $disable_right ? ' disabled' : ''; ?>" aria-disabled="<?php echo $disable_right ? 'true' : 'false'; ?>">&gt&gt</a>
-  <br>
+  <div class="m-2">
+    <a href="<?php echo FIRST; ?>" role="button" class="btn btn-primary m-1<?php echo $disable_left ? ' disabled' : ''; ?>" aria-disabled="<?php echo $disable_left ? 'true' : 'false'; ?>">&lt&lt</a>
+    <a href="<?php echo $data['left_date']; ?>" role="button" class="btn btn-primary m-1<?php echo $disable_left ? ' disabled' : ''; ?>" aria-disabled="<?php echo $disable_left ? 'true' : 'false'; ?>">&lt</a>
+    <a href="random-comic" role="button" class="btn btn-primary m-1">Random</a>
+    <a href="<?php echo $data['right_date']; ?>" role="button" class="btn btn-primary m-1<?php echo $disable_right ? ' disabled' : ''; ?>" aria-disabled="<?php echo $disable_right ? 'true' : 'false'; ?>">&gt</a>
+    <a href="<?php echo $data['latest_date']; ?>" role="button" class="btn btn-primary m-1<?php echo $disable_right ? ' disabled' : ''; ?>" aria-disabled="<?php echo $disable_right ? 'true' : 'false'; ?>">&gt&gt</a>
+  </div>
 
   <!-- Links to "dilbert.com" and the GitHub repo -->
-  <a href="<?php echo $permalink; ?>" target="_blank" role="button" class="btn btn-link my-3 mx-1">Permalink</a>
-  <div class="github mb-3">
-    <a href="<?php echo GITHUB_REPO; ?>" target="_blank" class="btn btn-light">
-      <span class="h5"><img src="static/github.png" height="24" width="24"></span>
-    </a>
-  </div>
-
-  </div>
+  <a href="<?php echo $permalink; ?>" target="_blank" role="button" class="btn btn-link m-1">Permalink</a>
+  <a href="<?php echo GITHUB_REPO; ?>" target="_blank" class="btn btn-light m-1">
+    <img src="static/github.png" height="24" width="24">
+  </a>
 </body>
 
 </html>
