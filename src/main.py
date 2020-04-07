0ff7a49f8ec9f9942c1577c37a9dc7b4fa2ec744
rharish101 <harish.rajagopals@gmail.com>
2020-04-07 11:25:38 +0530

    Now updating latest date on "dilbert.com" redirect
    If the latest date in the cache is older than a date that "dilbert.com"
    redirects to, then we cache this newer date.

--- src/main.py
+++ src/main.py
@@ -72,6 +72,51 @@ async def close_aux():
     await app.client_sess.close()
 
 
+async def _serve_template(date, data, latest_comic):
+    """Serve the HTML given scraped data.
+
+    Both input dates must be in the format used by "dilbert.com".
+
+    Args:
+        date (str): The (possibly corrected) date of the comic
+        data (dict): The scraped comic data
+        latest_comic (str): The date of the latest comic
+
+    Returns:
+        The rendered template for the comic page
+
+    """
+    date_obj = str_to_date(date)
+
+    # Links to previous and next comics
+    previous_comic = date_to_str(
+        max(str_to_date(FIRST_COMIC), date_obj - timedelta(days=1))
+    )
+    next_comic = date_to_str(
+        min(str_to_date(latest_comic), date_obj + timedelta(days=1))
+    )
+
+    # Whether to disable left/right navigation buttons
+    disable_left_nav = date == FIRST_COMIC
+    disable_right_nav = date == latest_comic
+
+    # Link to original strip on "dilbert.com"
+    permalink = SRC_PREFIX + date
+
+    return await render_template(
+        "layout.html",
+        data=data,
+        date=date,
+        first_comic=FIRST_COMIC,
+        previous_comic=previous_comic,
+        next_comic=next_comic,
+        disable_left_nav=disable_left_nav,
+        disable_right_nav=disable_right_nav,
+        permalink=permalink,
+        repo=REPO,
+    )
+
+
 async def serve_comic(date):
     """Serve the requested comic.
 
@@ -98,33 +143,22 @@ async def serve_comic(date):
     actual_date_obj = str_to_date(data["dateStr"], fmt=ALT_DATE_FMT)
     actual_date = date_to_str(actual_date_obj)
 
-    # Links to previous and next comics
-    previous_comic = date_to_str(
-        max(str_to_date(FIRST_COMIC), actual_date_obj - timedelta(days=1))
-    )
-    next_comic = date_to_str(
-        min(str_to_date(latest_comic), actual_date_obj + timedelta(days=1))
-    )
+    # This will contain awaitables for caching data (if required) and rendering
+    # the template. They are both independent of each other, and thus can be
+    # run in parallel.
+    todos = []
 
-    # Whether to disable left/right navigation buttons
-    disable_left_nav = actual_date == FIRST_COMIC
-    disable_right_nav = actual_date == latest_comic
+    # The date of the latest comic is often retrieved from the cache. If
+    # "dilbert.com" has redirected to a date which is newer than the cached
+    # value, then there is a new "latest comic". So cache the answer of
+    # "dilbert.com".
+    if str_to_date(latest_comic) < actual_date_obj:
+        latest_comic = actual_date
+        todos.append(app.latest_date_scraper.cache_data(actual_date))
 
-    # Link to original strip on "dilbert.com"
-    permalink = SRC_PREFIX + actual_date
-
-    return await render_template(
-        "layout.html",
-        data=data,
-        date=actual_date,
-        first_comic=FIRST_COMIC,
-        previous_comic=previous_comic,
-        next_comic=next_comic,
-        disable_left_nav=disable_left_nav,
-        disable_right_nav=disable_right_nav,
-        permalink=permalink,
-        repo=REPO,
-    )
+    todos.append(_serve_template(actual_date, data, latest_comic))
+    _, to_serve = await asyncio.gather(*todos)
+    return to_serve
 
 
 @app.route("/")
