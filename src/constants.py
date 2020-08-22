2c0e91ab0ad839ee7718a75a41b1b1c2ac7657de
Harish Rajagopal <harish.rajagopals@gmail.com>
2020-08-22 19:50:34 +0530

    Added slightly more explanation for comments

--- src/constants.py
+++ src/constants.py
@@ -4,9 +4,10 @@ from typing_extensions import Final
 # ==================================================
 # Date formats
 # ==================================================
-FIRST_COMIC: Final = "1989-04-16"  # date of the first Dilbert comic ever
-DATE_FMT: Final = "%Y-%m-%d"  # date format used for URLs
-ALT_DATE_FMT: Final = "%A %B %d, %Y"  # date format used for display
+FIRST_COMIC: Final = "1989-04-16"  # date of the first ever Dilbert comic
+DATE_FMT: Final = "%Y-%m-%d"  # date format used for URLs on "dilbert.com"
+# Date format used for display with the comic on "dilbert.com"
+ALT_DATE_FMT: Final = "%A %B %d, %Y"
 
 # ==================================================
 # Parameters for scraping from "dilbert.com"
@@ -19,13 +20,14 @@ FETCH_TIMEOUT: Final = 3
 # ==================================================
 # Parameters for caching to the database
 # ==================================================
-MAX_DB_CONN: Final = 10  # limit for connections to database
+MAX_DB_CONN: Final = 10  # limit for connections to the cache database
 # Timeout (in seconds) for a single database operation
 DB_TIMEOUT: Final = 3
 # Limit (in no. of comics) for the comics cache in the database. Heroku's free
-# tier limit is 10,000.
+# tier limit is 10,000 rows in a database. Note that apart from this, we have
+# the latest date table, which always has exactly one row.
 CACHE_LIMIT: Final = 9000
-# No. of hrs after scraping the latest date when it is to be queried again
+# No. of hrs after scraping the latest date when it is to be scraped again
 LATEST_DATE_REFRESH: Final = 2
 
 # ==================================================
@@ -33,5 +35,5 @@ LATEST_DATE_REFRESH: Final = 2
 # ==================================================
 # URL prefix for each comic on "dilbert.com"
 SRC_PREFIX: Final = "https://dilbert.com/strip/"
-# Link to the public repo; mainly for publicity :P
+# Link to the public version of this repo; mainly for publicity :P
 REPO: Final = "https://github.com/rharish101/dilbert-viewer"
